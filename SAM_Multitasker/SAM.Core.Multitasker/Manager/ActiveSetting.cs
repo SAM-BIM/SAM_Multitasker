914c3bd7b3503f3e6a4f3ef608c39b0f7832ef98
Michal Dengusiak <tassimulation@gmail.com>
2026-03-31 12:49:48 +0200

    Sow/2026 q1 (#4)
    * ActiveSetting loading bug fixed
    
    * Update spdx-check.yml
    
    * Codex Review and header added
    
    ---------
    
    Co-authored-by: Jakub Ziolkowski <jakubziolkowski@digiproject.uk>

--- SAM_Multitasker/SAM.Core.Multitasker/Manager/ActiveSetting.cs
+++ SAM_Multitasker/SAM.Core.Multitasker/Manager/ActiveSetting.cs
@@ -1,16 +1,23 @@
-﻿using System.Reflection;
+﻿// SPDX-License-Identifier: LGPL-3.0-or-later
+// Copyright (c) 2020–2026 Michal Dengusiak & Jakub Ziolkowski and contributors
+
+using System.Reflection;
 
 namespace SAM.Core.Multitasker
 {
     public static partial class ActiveSetting
     {
-        private static Setting setting = Load();
+        private static Setting setting = null;
+
+        private static readonly object settingLock = new ();
 
         private static Setting Load()
         {
             Setting setting = ActiveManager.GetSetting(Assembly.GetExecutingAssembly());
             if (setting == null)
+            {
                 setting = GetDefault();
+            }
 
             return setting;
         }
@@ -19,13 +26,18 @@ namespace SAM.Core.Multitasker
         {
             get
             {
+                lock (settingLock)
+                {
+                    setting ??= Load();
+                }
+
                 return setting;
             }
         }
 
         public static Setting GetDefault()
         {
-            Setting result = new Setting(Assembly.GetExecutingAssembly());
+            Setting result = new (Assembly.GetExecutingAssembly());
 
             ////File Names
             //result.SetValue(AnalyticalSettingParameter.DefaultMaterialLibraryFileName, "SAM_MaterialLibrary.JSON");
